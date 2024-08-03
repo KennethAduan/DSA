e75df44b4d5adca80ee3324a5256ab11770fde7b
Kineto <kenneth.aduan10@gmail.com>
2024-08-03 21:39:36 +0800

    i

--- recursion/index.ts
+++ recursion/index.ts
@@ -1,10 +1,24 @@
-function recursiveFibonacci(n: number) {
-  if (n < 2) return n;
+// Fibonacci number
+// function recursiveFibonacci(n: number) {
+//   if (n < 2) return n;
 
-  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+//   return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+// }
+
+// console.log(recursiveFibonacci(0));
+// console.log(recursiveFibonacci(1));
+// console.log(recursiveFibonacci(6));
+// console.log(recursiveFibonacci(7));
+
+// Factorial function
+function factorialRecursion(n: number) {
+  // Factorail formual recursion
+  // n! = n * (n - 1)!
+
+  if (n === 0) return 1;
+  return n * factorialRecursion(n - 1);
 }
 
-console.log(recursiveFibonacci(0));
-console.log(recursiveFibonacci(1));
-console.log(recursiveFibonacci(6));
-console.log(recursiveFibonacci(7));
+console.log(factorialRecursion(0));
+console.log(factorialRecursion(1));
+console.log(factorialRecursion(5));
