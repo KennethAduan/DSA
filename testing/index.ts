0e6104e5f5f218a28575f692afbc16d1fe6a3873
Kenneth Aduan <kenneth.aduan10@gmail.com>
2024-08-04 17:03:14 +0800

    add number palindrome

--- testing/index.ts
+++ testing/index.ts
@@ -49,15 +49,45 @@
 
 // console.log(isPrime(5)); // Output: true
 // console.log(isPrime(4)); // Output: false
-function twoSum(nums: number[], target: number) {
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = i; j < nums.length; j++) {
-      if (nums[j] + nums[j - i] === target) {
-        return [j - i, j];
-      }
-    }
+// function twoSum(nums: number[], target: number) {
+//   for (let i = 1; i < nums.length; i++) {
+//     for (let j = i; j < nums.length; j++) {
+//       if (nums[j] + nums[j - i] === target) {
+//         return [j - i, j];
+//       }
+//     }
+//   }
+//   return []; // Empty array if no solution found
+// }
+
+// console.log(twoSum([2, 7, 11, 15], 9));
+
+// function isPalindrome(x: number): boolean {
+//   const convert = Number(x.toString().split("").reverse().join(""));
+
+//   if (convert === x) {
+//     return true;
+//   }
+
+//   return false;
+// }
+
+// console.log(isPalindrome(121));
+function isPalindrome(x: number): boolean {
+  // Handle negative numbers and numbers ending in 0
+  if (x < 0 || (x % 10 === 0 && x !== 0)) {
+    return false;
   }
-  return []; // Empty array if no solution found
+
+  let reversed = 0;
+
+  while (x > reversed) {
+    reversed = reversed * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  // Handle odd length palindromes
+  return x === reversed || x === Math.floor(reversed / 10);
 }
 
-console.log(twoSum([2, 7, 11, 15], 9));
+console.log(isPalindrome(121)); // Output: true
